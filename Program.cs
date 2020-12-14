637f1fe5f58d39b96020f9df79a6f76c6d7240b4
giovanni iannace <giovanniiannace56@gmail.com>
2020-12-14 14:59:44 -0300

    calculadora de moedas feita

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using aula_14_12.classes;
+
+namespace aula_14_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine(Math.Pow(2,5));
+
+            Console.WriteLine("Digite um valor");
+            Conversor.valorUsuario = float.Parse(Console.ReadLine());
+            Console.WriteLine("valor de dolar para real é : ");
+            Console.WriteLine(Conversor.ConverterDolarParaReal());
+            Console.WriteLine("valor de real para dolar é : ");
+            Console.WriteLine(Conversor.ConverterRealParaDolar());
+            Console.WriteLine("valor de euro para para real é : ");
+            Console.WriteLine(Conversor.CoverterEuroParaReal());
+            Console.WriteLine("valor de real para euro é : ");
+            Console.WriteLine(Conversor.ConverterRealParaEuro());
+
+            Testar();
+        }
+
+        static void Testar(){
+
+        }
+    }
+}
